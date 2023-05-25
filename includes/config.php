66b1f193d0e31c8e78e9f8e4f8f2f0c8babf29aa
MohamedAlaa006 <30202016003@alexu.edu.eg>
2023-05-25 18:28:21 +0300

    initial commit

---
+++ includes/config.php
@@ -0,0 +1,11 @@
+<?php
+$config['db']['host'] = 'localhost';
+$config['db']['name'] = 'quicklancer';
+$config['db']['user'] = 'root';
+$config['db']['pass'] = '';
+$config['db']['pre'] = '';
+
+$config['admin_folder'] = 'admin';
+$config['version'] = '2.4';
+$config['installed'] = '1';
+?>
